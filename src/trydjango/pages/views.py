a90e43bce24d9d449f1a8f1c7c140945772d0c43
Harun Ibram <ibramharun7@gmail.com>
2024-02-19 23:35:56 +0200

    day2

--- src/trydjango/pages/views.py
+++ src/trydjango/pages/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from pathlib import Path
 # Create your views here.
 
 def home_view(request, *arg, **kwargs):
@@ -7,8 +8,16 @@ def home_view(request, *arg, **kwargs):
 	return render(request, "home.html", {})
 
 def uwu_view(request, *arg, **kwargs):
-	# return HttpResponse("<h2>uwu~!</h2>")
-	return render(request, "uwu.html", {})
+	BASEDIR = Path(__file__).resolve().parent
+	akali = Path.joinpath(BASEDIR, 'akali.jpg')
+	my_context = {
+		"my_name": "art",
+		"my_number": 69,
+		"my_list": ["this", "is", "super", "cool"],
+		"my_tuple": ("oh", "no"),
+		"my_akali": akali
+	}
+	return render(request, "uwu.html", my_context)
 
 def test_view(request, *arg, **kwargs):
 	return render(request, "test.html", {})
